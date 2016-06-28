29ee4e03e4d138886f523acb95259bc680d453ad
Natalie <natzsimpson@hotmail.com>
2016-06-28 15:27:32 +0100

    adds index

--- booking_controller.rb
+++ booking_controller.rb
@@ -6,3 +6,14 @@ require_relative('./models/booking')
 get '/booking/new' do
   erb( :new )
 end  
+
+post '/booking' do
+  @booking = Booking.new(params)
+  @booking.save()
+  erb( :create )
+end
+
+get '/booking' do
+  @booking = Booking.all()
+  erb( :index )
+end    
